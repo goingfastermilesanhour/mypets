fe013f3fc72ea12c4437e6df112bc9096281ed5d
Razvan Gheghe <razvan.gheghe@gmail.com>
2024-04-15 19:01:38 +0300

    added import pictures functionality and delete all

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,32 @@ import PetComponent from "./components/PetComponent.jsx";
 import TabMeetMe from "./components/tabMeetme.jsx";
 import { MEETME } from "./data.js";
 import { useState } from "react";
-
+import { useRef } from "react";
 
 function App() {
   // pentru state:
   const [selectedTab, setSelectedTab] = useState();
-console.log(selectedTab)
+  const [file, setFile] = useState();
+  const [imagesPreview, setImagesPreview] = useState([]);
+
   function handleSelect(ceamselectat) {
     setSelectedTab(ceamselectat);
   }
 
-
-
+  const fileInputRef = useRef();
+  const handleUpload = (event) => {
+    setFile(event.target.files[0])
+    const newArray = [...imagesPreview, URL.createObjectURL(event.target.files[0]) ];
+    setImagesPreview(newArray)
+  } 
   return (
     <div>
       <Header />
       <main>
+        
         <section id="ManagingState">
           <h2>Get to know me(folosesc useState aici)</h2>
+          {!selectedTab && <p>Please get to know my pets</p>}
           <menu>
             <TabMeetMe onSelect={() => handleSelect('Harbuz')}>Harbuz</TabMeetMe>
             <TabMeetMe onSelect={() => handleSelect('Charlie')}>Charlie</TabMeetMe>
@@ -30,7 +38,7 @@ console.log(selectedTab)
             <TabMeetMe onSelect={() => handleSelect('Lola')}>Lola</TabMeetMe>
             <TabMeetMe onSelect={() => handleSelect('Rocky')}>Rocky</TabMeetMe>
           </menu>
-          {!selectedTab && <p>Please get to know my pets</p>}
+          
           {selectedTab && (
             <div id="tab-content">
               <h3>{MEETME[selectedTab].title}</h3>
@@ -46,6 +54,19 @@ console.log(selectedTab)
             {pets_info.map((petComponent) => <PetComponent key={PetComponent.title} {...petComponent} />)}
           </ul>
         </section>
+
+        <section id="addNew" >
+          <button onClick={()=>fileInputRef.current.click()} type="button">Import a picture of your own pet</button>
+          {file &&
+          <button onClick={()=>{setFile(null); setImagesPreview([])}} type="button">Delete all pictures</button>
+          }
+          <input onChange={handleUpload} multiple={false} ref={fileInputRef} type="file"hidden></input>
+        </section>
+        <div className="picturesContainer">
+        {imagesPreview.map((image) => {
+            return <img width="130px" height="200px" src={image}></img>
+          })}        
+        </div>
       </main>
     </div>
   );
